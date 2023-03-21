a57dbf13e9f184a834a0bca9370e29aa56b6e80f
Lucas Bittencurt <lucasfilipeb@gmail.com>
2023-03-21 15:57:43 -0300

    class 40 update

--- curso_python1/aula40.py
+++ curso_python1/aula40.py
@@ -2,9 +2,12 @@
 while True:
     number_1 = input('Type a number: ')
     number_2 = input('Type other: ')
-    number_3 = input('Type operator (+-/*): ')
+    operator = input('Type operator (+-/*): ')
     
     valid_numbers = None
+
+    number_1_float = 0
+    number_2_float = 0
 
     try:
         number_1_float = float(number_1)
@@ -15,11 +18,30 @@ while True:
 
     if valid_numbers is None:
         print('One or more number are invalid.')
-        continue       
+        continue
 
-    sair = input('Quer sair? [s]im: ')
-    if sair.lower().startswith('s'):
-        print(sair)
+    valid_operators = '+-/*'
+
+    if operator not in valid_operators or len(operator) != 1:
+        print('Invalid operator.')
+        continue
+
+    print('Calculating...')
+    if operator == '+':
+        print(f'Result: {number_1_float + number_2_float}')
+    elif operator == '-':
+        print(f'Result: {number_1_float - number_2_float}')
+    elif operator == '/':
+        print(f'Result: {number_1_float / number_2_float}')
+    elif operator == '*':
+        print(f'Result: {number_1_float * number_2_float}')
+    else:
+        print('Unexpected error.')
+        continue
+
+    exit = input('Do you want to exit? [y]es: ')
+    if exit.lower().startswith('y'):
+        print(exit)
         break
 
 print('system end')
